a426783cde9a170d6b221bc6eec22df35bd09e04
Mario Arranz <marranz.agueda@gmail.com>
2018-11-22 13:48:47 +0100

    make it better

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const Botgram = require('botgram');
 const figlet = require('figlet');
-require('./secrets');
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-console.log(TELEGRAM_BOT_TOKEN);
+require('dotenv').config();
 
-if (!TELEGRAM_BOT_TOKEN) {
+if (!process.env.TELEGRAM_BOT_TOKEN) {
   console.error('Seems like you forgot to pass Telegram Bot Token. I can not proceed...');
   process.exit(1);
 }
 
-const bot = new Botgram(TELEGRAM_BOT_TOKEN);
+const bot = new Botgram(process.env.TELEGRAM_BOT_TOKEN);
 
 function onMessage(msg, reply) {
   figlet(msg.text, (err, data) => {
